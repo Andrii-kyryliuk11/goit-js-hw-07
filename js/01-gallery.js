0ed484880b2e0f1c3995638b4069abacc8b4dfb7
Andrii-kyryliuk11 <poteriana@gmail.com>
2023-01-10 19:23:28 +0100

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+function createGalleryMarkup(galleryItems) {
+  const arrayOfMarkup = [];
+
+  galleryItems.forEach((image) => {
+    arrayOfMarkup.push(
+      `<div class="gallery__item">
+  <a target="_self" class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`
+    );
+  });
+
+  refs.gallery.innerHTML = arrayOfMarkup.join("");
+}
+
+createGalleryMarkup(galleryItems);
+
+refs.gallery.addEventListener("click", onGalleryElementClick);
+
+function onGalleryElementClick(e) {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  e.preventDefault();
+
+  const originalImageRef = e.target.dataset.source;
+
+  openModal(originalImageRef);
+}
+
+function openModal(ref) {
+  const instance = basicLightbox.create(
+    `<img src="${ref}" width="800" height="600">`
+  );
+  instance.show();
+
+  window.addEventListener("keydown", (e) => {
+    if (e.keyCode === 27) {
+      instance.close();
+      console.log(e);
+      window.removeEventListener("keydown", e);
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+function createGalleryMarkup(galleryItems) {
+  const arrayOfMarkup = [];
+
+  galleryItems.forEach((image) => {
+    arrayOfMarkup.push(
+      `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`
+    );
+  });
+
+  refs.gallery.innerHTML = arrayOfMarkup.join("");
+}
+
+createGalleryMarkup(galleryItems);
+
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
+gallery.on("show.simplelightbox", function () {});
